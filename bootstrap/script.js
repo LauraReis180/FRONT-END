7872496f4d05ad22ac938c1a8cd8c384603826da
laurareis <lauradasilvareis09@gmail.com>
2024-12-16 15:23:15 -0300

    receitas

--- bootstrap/script.js
+++ bootstrap/script.js
@@ -9,11 +9,11 @@ class Categoria {
     }
 }
 
-lista_categorias.push(new Categoria('SUCOS','https://i.pinimg.com/236x/7a/c7/62/7ac76299b0add0b815b6160e792ffe95.jpg','Suco para todos os gostos, frescor em cada sabor!','Suco feito por @Blendy'))
+lista_categorias.push(new Categoria('SUCOS','images/drinks.','Suco para todos os gostos, frescor em cada sabor!','Suco feito por @Blendy'))
 
 lista_categorias.push(new Categoria('SMOOTHIES','https://domf5oio6qrcr.cloudfront.net/medialibrary/8931/Smoothie-post.jpg','Smoothies: todos os sabores,para todos os momentos!','feito por @Blendy'))
 
-lista_categorias.push(new Categoria('DRINKS ','https://revistasaboresdosul.com.br/wp-content/uploads/2022/08/majestic-gastronomia4.jpg','Sabor que refresca! Descubra nossos drinks deliciosos e inovadores, feitos para todos os gostos!','feito por @Blendy'))
+lista_categorias.push(new Categoria('DRINKS ','images/drink.png','Sabor que refresca! Descubra nossos drinks deliciosos e inovadores, feitos para todos os gostos!','feito por @Blendy'))
 
 lista_categorias.push(new Categoria('REFRIGERANTES NATURAIS','http://127.0.0.1:5500/images/MacBook%20Air%20-%201%20(9).png','Refresque-se com o melhor da natureza! Nossos refrigerantes naturais trazem o sabor fresco e saudável que você procura.','feito por @Blendy'))
 
