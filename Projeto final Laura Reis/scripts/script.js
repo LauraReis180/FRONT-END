3dc8a963fb36989153509b5ca96de900edeacf23
LauraReis180 <lauradasilvareis09@gmail.com>
2024-12-18 02:10:16 -0300

    PROJETO

--- Projeto final Laura Reis/scripts/script.js
+++ Projeto final Laura Reis/scripts/script.js
@@ -1,15 +1,7 @@
-// Seleciona os elementos
-const loginBtn = document.getElementById('login-btn');
-const loginBox = document.getElementById('login-box');
-
-// Função para exibir a caixa de login
-loginBtn.addEventListener('click', () => {
-    loginBox.style.display = 'block';
-});
-
-// Fechar a caixa de login quando o usuário clicar em qualquer área fora dela (opcional)
-window.addEventListener('click', (e) => {
-    if (e.target === loginBox) {
-        loginBox.style.display = 'none';
-    }
-});
+// Função para destacar o botão clicado
+function highlightButton(button) {
+    let buttons = document.querySelectorAll('.btn');
+    buttons.forEach(b => b.classList.remove('active'));
+    button.classList.add('active');
+  }
+  
