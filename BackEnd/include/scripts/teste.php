c79eab1ac063d4f43c2260af6a5b891e7dab34de
laurareis180 <lauradasilvareis09@gmail.com>
2025-03-10 17:38:32 -0300

    BACK END
    JOGOS

---
+++ BackEnd/include/scripts/teste.php
@@ -0,0 +1,17 @@
+<?php
+     echo "Hello World!!!";
+     $repetir = true;
+     $vezes = 10;
+     $contador = 1;
+
+     while ($repetir == true){
+        echo " <br> Estou repetindo";
+
+        if($contador == $vezes){
+        //break;
+        $repetir = false;
+            
+        }
+        $contador++;
+     }
+    ?>
