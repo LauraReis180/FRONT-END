1b2ec39fa69d2949a8d23fb8e80963d5c807918f
LauraReis180 <lauradasilvareis09@gmail.com>
2025-03-09 23:05:20 -0300

    livro
    pagina

--- BackEnd/livros/index.php
+++ BackEnd/livros/index.php
@@ -1,14 +1,44 @@
 <!DOCTYPE html>
-<html lang="pt - br">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>livros</title>
+    <link rel="stylesheet" href="style/index.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 </head>
 <body>
-    <h1> LRlivros</h1>
+    <header>
+    <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+  <div  class="navbar-brand mx-auto" href="#">Livraria Digital</a>
+</div>
 
-    <p> </p>
-    
+
+</nav>
+</header>
+
+<main>
+  <div class="card mb-3" style="max-width: 590px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="images/pequeno principe.jpg" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">O Pequeno Príncipe </h5>
+        <p class="card-text">O Pequeno Príncipe é um livro escrito por Antoine de Saint-Exupéry que narra a história de um piloto que encontra um menino no deserto do Saara. O livro é um dos mais conhecidos do mundo e um clássico da literatura universal. </p>
+        <p><strong>AUTOR:</strong> Antoine de Saint-Exupéry </p>
+        <a href="#" class="btn btn-primary">Adquira já</a>
+      </div>
+      </div>
+  </div>
+</div>
+</main>
+
+<footer>
+  <p>&copy; 2024 Livraria Virtual </p>
+</footer>
+
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </html>
