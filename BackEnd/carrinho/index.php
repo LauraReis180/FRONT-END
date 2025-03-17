f2d227673a7b172ad0c4c5f0bfbefef01b2a9c3a
laurareis180 <lauradasilvareis09@gmail.com>
2025-03-17 17:23:31 -0300

    php
    carrinho

--- BackEnd/carrinho/index.php
+++ BackEnd/carrinho/index.php
@@ -29,6 +29,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="index.css">
 </head>
 <body>
 
