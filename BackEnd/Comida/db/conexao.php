d5a4ead0664e83cc4243f99a4d5b7aa9e172e054
laurareis180 <lauradasilvareis09@gmail.com>
2025-04-01 16:56:24 -0300

    php
    banco de dados

---
+++ BackEnd/Comida/db/conexao.php
@@ -0,0 +1,4 @@
+<?php
+  include("db/config.php");
+   $conexao = mysqli_connect(SERVIDOR,USUARIO,SENHA,BANCO) or die ("Erro na conexão com o serviço!" .mysqli_connect_error());
+?>
---
+++ BackEnd/Comida/index.php
@@ -0,0 +1,26 @@
+<?php
+    include('db/conexao.php');
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alta gastromidade</title>
+</head>
+<body>
+    <header>
+        
+       
+            
+        
+    </header>
+
+    <main>
+        
+        
+    </main>
+   
+</body>
+</html>
