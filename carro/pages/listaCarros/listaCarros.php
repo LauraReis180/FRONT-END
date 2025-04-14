d4322d4c9fa9e7eeaf51c4bd83196447bf1d5339
LauraReis180 <lauradasilvareis09@gmail.com>
2025-04-14 17:34:36 -0300

    php
    carros

--- carro/pages/listaCarros/listaCarros.php
+++ carro/pages/listaCarros/listaCarros.php
@@ -1,40 +1,61 @@
-<h1>VOCE ESTÁ NA LISTA DE CARROS</h1>
+<h1>VOCÊ ESTÁ NA LISTA DE CARROS</h1>
 
-<table>
+<a href="index.php?menu=adicionarCarro">
+    <button type="button">ADICIONAR CARRO</button>
+</a>
+
+<form action="index.php?menu=lista" method="post">
+  <Input type="text" name="pesquisa" id="pesquisa">
+<button type="submit">PESQUISA</button>
+
+</Input>
+</form>
+
+  buttom type"submit"></form>
+
+<table class="table">
     <tr>
         <th>Modelo</th>
         <th>Marca</th>
         <th>Valor</th>
-        <th>Ano </th>
+        <th>Ano</th>
         <th>Cor</th>
     </tr>
-
-
-
-
-
-
-
-<?php
-  $sql = "SELECT *FROM carros";
-  // pedido
-  $query = mysqli_query($conexao,$sql) or die("Erro na requisição!".mysqli_error($conexao));
- 
-// fetch_asso = vai acessar um query, e contar os resultados
-  while($dados = mysqli_fetch_assoc($query)){ 
-    ?>
-    <tr>
-        <td><?=$dados['modeloCarro']?></td>
-        <td><?=$dados['marcaCarro']?></td>
-        <td><?=$dados['valorCarro']?></td>
-        <td><?=$dados['anoCarro']?></td>
-        <td><?=$dados['corCarro']?></td>
-        <td><a href="index.php?menu=editarCarro&idCarro=<?=$dados['idCarro']?>" class="btn btn-primary">EDITAR</a></td>
-    </tr>
-    
     <?php
+    if(isset($_POST[PESQUISA])){
+      $ter
+    }
+        $sql="SELECT idCarro,
+        upper(modeloCarro) AS modeloCarro,
+        upper(marcaCarro) AS marcaCarro,
+        upper(valorCarro) AS valorCarro,
+        DATE_FORMAT(anoCarro, '%d/%m/%y') AS anoCarro,
+        upper(corCarro) AS corCarro 
+        FROM carros
+        idCarro ='$termoPesquisado}' OR
+        modeloCarro LIKE '%$termoPesquisado%'
+        ORDER BY modeloCarro ASC'
+      
+      ";
+        
+        // pedido
+        $query = mysqli_query($conexao,$sql) or die("Erro na requisição!".mysqli_error($conexao));
 
-  }
-?>
+        // fetch_asso = vai acessar um query, e contar os resultado
+
+        while($dados = mysqli_fetch_assoc($query)){
+            ?>
+                <tr>
+                    <td><?=$dados['modeloCarro']?></td>
+                    <td><?=$dados['marcaCarro']?></td>
+                    <td><?=$dados['valorCarro']?></td>
+                    <td><?=$dados['anoCarro']?></td>
+                    <td><?=$dados['corCarro']?></td>
+                    <td><a href="index.php?menu=editarCarro&idCarro=<?=$dados['idCarro']?>" class="btn btn-primary">EDITAR</a></td>
+                    <td><a href="index.php?menu=deletarCarro&idCarro=<?=$dados['idCarro']?>" class="btn btn-danger">DELETAR</a></td>
+                </tr>
+            <?php
+        }
+    ?>
 
 </table>
