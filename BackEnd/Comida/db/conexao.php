3928badc0ed88fe06acf66d34f3bc82b6f8fd3a9
LauraReis180 <lauradasilvareis09@gmail.com>
2025-04-19 19:53:26 -0300

    PHP
     SENAI

--- BackEnd/Comida/db/conexao.php
+++ BackEnd/Comida/db/conexao.php
@@ -1,3 +1,7 @@
 <?php
-  include("db/config.php");
-   $conexao = mysqli_connect(SERVIDOR,USUARIO,SENHA,BANCO) or die ("Erro na conexão com o serviço!" .mysqli_connect_error());
+include("db/config.php");
+
+// mysqli_connect = conecta o projeto com o banco
+$conexao = mysqli_connect(SERVIDOR, USUARIO, SENHA, BANCO) 
+    or die("Erro na conexão com o serviço: " . mysqli_connect_error());
+?>
--- BackEnd/Comida/db/config.php
+++ BackEnd/Comida/db/config.php
@@ -1,7 +1,7 @@
 <?php
-
- DEFINE ("SERVIDOR","localhost");
- DEFINE ("USUARIO","root");
- DEFINE ("SENHA","");
- DEFINE ("BANCO","comida");
+// CONSTANTES QUE SALVAM A CONFIGURAÇÃO DE CONEXÃO COM O BANCO
+define("SERVIDOR", "localhost:3306");
+define("USUARIO", "root");
+define("SENHA", "laura18");
+define("BANCO", "comida");
 ?>
