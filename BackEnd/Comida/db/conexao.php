03c185756d71da47766781b9ecef9ed8b8c855d9
LauraReis180 <lauradasilvareis09@gmail.com>
2025-04-20 02:39:28 -0300

    comida
    senai

--- BackEnd/Comida/db/conexao.php
+++ BackEnd/Comida/db/conexao.php
@@ -1,6 +1,4 @@
 <?php
     include("DB/config.php");
-
-    // mysqli_con = conecta o projeto com o banco
-    $conexao = mysqli_connect(SERVIDOR,USUARIO,SENHA,BANCO) or die("Erro na conexão com o serviço!".mysqli_connect_error());
+$conexao = mysqli_connect(SERVIDOR,USUARIO,SENHA,BANCO) or die("Erro na conexão com o serviço!".mysqli_connect_error());
 ?>
